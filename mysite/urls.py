bd43b8690fe52358c0eb91b427462df7df478f65
Sushil Kumar <sushilitdeveloper@gmail.com>
2021-09-22 18:58:42 +0530

    changes

--- mysite/urls.py
+++ mysite/urls.py
@@ -21,5 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
     path('accounts/login/', LoginView.as_view(), name='login'),
-    path('accounts/logout/', LogoutView, name='logout', kwargs={'next_page':'/'})
+    path('accounts/logout/', LogoutView.as_view(next_page='/'), name='logout')
 ]
